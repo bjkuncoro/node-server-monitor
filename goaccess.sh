33990cb1dad891a0fec5b0cee069feb2d5a13f3d
bjkuncoro <bjkuncoro@untan.ac.id>
2017-11-16 14:37:59 +0700

    fuck

--- goaccess.sh
+++ goaccess.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 while true
 do
- goaccess /var/log/nginx/access.log -a -d -o /var/www/html/belajarnode/belajar-node/public/laporan.json
+ goaccess /var/log/nginx/access.log -a -d -o /var/www/html/belajarnode/mainscript/public/laporan.json
  sleep 3
 done
